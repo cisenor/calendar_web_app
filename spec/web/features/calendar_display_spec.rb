070f4c63bd7d7765f0b7d806f490354dac15f874
craig <craig@stembolt.com>
2018-03-21 15:52:31 -0700

    Incorporate the Year and Month classes. Add test respectively.

---
+++ spec/web/features/calendar_display_spec.rb
@@ -0,0 +1,5 @@
+require 'features_helper'
+
+describe 'Show calendar' do
+  let(:year) { Year.new(2018) }
+end
--- spec/web/views/home/index_spec.rb
+++ spec/web/views/home/index_spec.rb
@@ -6,15 +6,15 @@ describe Web::Views::Home::Index do
   let(:view)      { Web::Views::Home::Index.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'has 1 year on the index page' do 
-    page.has_css?('.year', count: 1)
+  it 'has 1 year on the index page' do
+    rendered.scan('class="year"').count.must_equal 1
   end
 
   it 'has 12 months on the index page' do
-    page.has_css?('.month', count: 12)
+    rendered.scan('class="month"').count.must_equal 12
   end
 
   it 'has a form to add a calendar entry' do
-    page.has_css?('form#add-entry', count: 1)
+    rendered.scan('class="add-entry-form"').count.must_equal 1
   end
 end
