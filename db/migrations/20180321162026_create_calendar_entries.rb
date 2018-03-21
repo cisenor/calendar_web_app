148acb3cea2406524dc62d97b2ef5d1a82579472
craig <craig@stembolt.com>
2018-03-21 11:42:11 -0700

    Add tests for date creation

--- db/migrations/20180321162026_create_calendar_entries.rb
+++ db/migrations/20180321162026_create_calendar_entries.rb
@@ -6,7 +6,10 @@ Hanami::Model.migration do
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
       column :name, String, null: false
-
+      column :month, Integer, null: false
+      column :day, Integer, null: true
+      column :occurrence_week, Integer, null: true
+      column :occurrence_weekday, Integer, null: true
     end
   end
 end
--- spec/calendar/entities/calendar_entry_spec.rb
+++ spec/calendar/entities/calendar_entry_spec.rb
@@ -1,13 +1,24 @@
 require_relative '../../spec_helper'
 
 describe CalendarEntry do
+  christmas = CalendarEntry.new(name: 'Chrimbo', month: 12, day: 25)
+  easter = CalendarEntry.new(name: 'Easter', month: 4, occurrence_week: 1, occurrence_weekday: 1)
+  thanksgiving = CalendarEntry.new(name: 'Thanksgiving', month: 10, occurrence_week: 2, occurrence_weekday: 1)
   it 'can be created with concrete day & month' do
-    entry = CalendarEntry.new(name: 'Chrimbo', month: 12, day: 25)
-    entry.name.must_equal 'Chrimbo'
-    entry.month.must_equal 12
-    entry.day.must_equal 25
+    christmas.name.must_equal 'Chrimbo'
+    christmas.month.must_equal 12
+    christmas.day.must_equal 25
   end
-  describe 'can be created with week and weekday occurrence' do
-    entry = CalendarEntry.new(name: 'Easter', month: 4, occurrence_week: 1, occurrence_weekday: 1)
+  it 'can be created with week and weekday occurrence' do
+    easter.name.must_equal 'Easter'
+    easter.month.must_equal 4
+    easter.occurrence_week.must_equal 1
+    easter.occurrence_weekday.must_equal 1
+  end
+  it 'can return date object' do
+    christmas.date(2015).must_equal Date.new(2015, 12, 25)
+    easter.date(2018).must_equal Date.new(2018, 4, 2)
+    thanksgiving.date(2018).must_equal Date.new(2018, 10, 8)
+    thanksgiving.date(2017).must_equal Date.new(2017, 10, 9)
   end
 end
