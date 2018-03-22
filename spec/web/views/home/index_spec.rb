f82cb67a06c8414ac444b6240cb740b959996001
Craig Isenor <toast@Craigs-iMac.office.stembolt.io>
2018-03-22 12:00:56 -0700

    Fixed missing end and changed from must_match to scan

--- spec/web/views/home/index_spec.rb
+++ spec/web/views/home/index_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../../../spec_helper'
 
 describe Web::Views::Home::Index do
-  let(:exposures) { Hash[year: Year.new(2018)] }
+  let(:exposures) { Hash[year: Year.new(2018), formatter: FormatFactory.new.create('')] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/home/index.html.erb') }
   let(:view)      { Web::Views::Home::Index.new(template, exposures) }
   let(:rendered)  { view.render }
 
   it 'has proper header' do
-    rendered.must_contain? '<div class="title">Calendar</div>'
+    rendered.scan('<div class="title">Calendar</div>').count.must_equal 1
+  end
+
   it 'has 1 year on the index page' do
     rendered.scan('class="year"').count.must_equal 1
   end
