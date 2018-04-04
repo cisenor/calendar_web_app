c49d863f2960265b7b48c12b80fa77c629426ae6
Craig Isenor <toast@Craigs-iMac.local>
2018-04-03 17:21:02 -0700

    Fixed a test failure.

--- spec/web/features/calendar_display_spec.rb
+++ spec/web/features/calendar_display_spec.rb
@@ -4,28 +4,28 @@ describe 'Show calendar' do
   let(:year) { Year.new(2018) }
   it 'defaults to current year' do
     visit '/'
-    year = page.find('#title-year').text
+    year = page.find('.title').text
     assert year == '2018', 'expected 2018, got ' + year
   end
 
   describe 'when passed a year as a router variable' do
     it 'presents the calendar for that year' do
       visit '/2017'
-      assert page.find('#title-year').text == '2017'
+      assert page.find('.title').text == '2017'
     end
   end
 
   it 'advances to the next year when the next button is clicked' do
     visit '/'
-    assert page.find('#title-year').text == '2018'
-    page.find('#next-year').click()
-    assert page.find('#title-year').text == '2019'
+    assert page.find('.title').text == '2018'
+    page.find('#next-year').click
+    assert page.find('.title').text == '2019'
   end
 
   it 'returns to the previous year when the previous button is clicked' do
     visit '/'
-    assert page.find('#title-year').text == '2018'
-    page.find('#previous-year').click()
-    assert page.find('#title-year').text == '2017'
+    assert page.find('.title').text == '2018'
+    page.find('#previous-year').click
+    assert page.find('.title').text == '2017'
   end
 end
