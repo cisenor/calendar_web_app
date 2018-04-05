d9091dbaf75221ac4fc5ae5af3462bbe6a800d74
Craig Isenor <toast@Craigs-iMac.office.stembolt.io>
2018-04-05 12:21:48 -0700

    Add public/private switch on date create form. Tweaked some css.

--- apps/web/assets/javascripts/form_values.js
+++ apps/web/assets/javascripts/form_values.js
@@ -1,3 +1,3 @@
-document.getElementById("calendar-entry-month").addEventListener("change", function(value,a ,bv){
-  document.getElementById("calendar-entry-day").max = new Date(2000, value.target.selectedIndex + 1, 0).getDate();
+document.getElementById("calendar-entry-fixed-month").addEventListener("change", function(value,a ,bv){
+  document.getElementById("calendar-entry-fixed-day").max = new Date(2000, value.target.selectedIndex + 1, 0).getDate();
 })
